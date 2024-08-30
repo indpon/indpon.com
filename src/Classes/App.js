c2d7ce55ece268626507c3ae79cad6a4293ce12c
= <=>
2024-08-29 23:13:38 -0400

    todo

--- src/Classes/App.js
+++ src/Classes/App.js
@@ -1,15 +1,30 @@
-// import logo from './logo.svg';
+import React, { useState } from 'react'; // Import useState
 import '../Static/App.css';
-import Clicker from './Clicker'
-import User from './User.js'
+import Clicker from './Clicker';
+import User from './User.js';
+
 
 
 function App() {
+  // Inside your App component
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = () => {
+    if (input.trim()) { // Make sure the input is not empty
+      setTodos([...todos, input]); // Add the new todo to the list
+      setInput(''); // Clear the input field
+    }
+  };
+
   return (
     <div>
-      <p>Hello welcome to indpon.com lolololol</p>
-      <Clicker />
-      <User />
+      <input
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTodo}>Add Todo</button>
     </div>
   );
 }
