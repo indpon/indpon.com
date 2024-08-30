1cbbc26b0b8ff2f5cc0ea96ee05ebae315b6b20a
= <=>
2024-08-30 00:30:19 -0400

    fix route (fix) (fix) (fix) (fix)

--- src/Classes/App.js
+++ src/Classes/App.js
@@ -4,33 +4,6 @@ import '../Static/App.css'; // Ensure you have this file for styling
 import About from './About';
 import CreateUser from './CreateUser'
 
-function Home() {
-  return (
-    <Router>
-      <header>
-        <h1>Welcome to indpon.com</h1>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/create/user">Create User</Link>
-          <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/K5rxmxvT83">Indpon's discord server</a>
-        </nav>
-      </header>
-
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/create/user" element={<CreateUser />} />
-        </Routes>
-      </main>
-
-      <footer>
-        <p>&copy; 2024 indpon.com</p>
-      </footer>
-    </Router>
-  );
-}
 
 
 function App() {
@@ -48,7 +21,6 @@ function App() {
 
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/create/user" element={<CreateUser />} />
         </Routes>
