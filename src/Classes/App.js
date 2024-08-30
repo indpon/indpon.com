1da14ec744a191e862dee59e270110bb19060928
= <=>
2024-08-30 01:03:54 -0400

    fix

--- src/Classes/App.js
+++ src/Classes/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import '../Static/App.css'; // Ensure you have this file for styling
 import About from './About';
 import CreateUser from './CreateUser';
 import Todos from './Todos';
 
 function Header() {
-  const location = useLocation();
-  // Only render header if the current path is "/"
-  return location.pathname !== null ? (
+  return (
     <header>
       <h1>Welcome to indpon.com</h1>
       <nav>
         <Link to="/about">About</Link>
         <Link to="/create/user">Create User</Link>
-        <Link to="todo">Indpon Todo List</Link>
+        <Link to="/todo">Indpon Todo List</Link>
         <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/K5rxmxvT83">Indpon's discord server</a>
       </nav>
     </header>
-  ) : null;
+  );
 }
 
 function App() {
@@ -33,8 +31,8 @@ function App() {
         </Routes>
       </main>
       <section>
-            <h2>Welcome to indpon.com</h2>
-            <p>(a website made by indpon)</p>
+        <h2>Welcome to indpon.com</h2>
+        <p>(a website made by indpon)</p>
       </section>
       <footer>
         <p>&copy; 2024 indpon.com</p>
