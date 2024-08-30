e3aa93117af33b0481e323b62b4e1cd3670551a2
= <=>
2024-08-30 12:00:04 -0400

    toString and fix css)

--- api/v1/createuser.js
+++ api/v1/createuser.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-    res.status(200).json({ message: 'Hey bud. This feature isnt done yet. ' + req})
+    res.status(200).json({ message: 'Hey bud. This feature isnt done yet. ' + req.toString()})
 }
