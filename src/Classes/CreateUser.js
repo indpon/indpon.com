fdb9c158feab6804e62cd28e8bc8bc674a697f19
= <=>
2024-08-30 11:49:08 -0400

    add old CreateUser ui.

--- src/Classes/CreateUser.js
+++ src/Classes/CreateUser.js
@@ -1,17 +1,23 @@
 import React from 'react';
 
+import '../Static/CreateUser.css'
+
 function CreateUser() {
     return (
         <div>
-            <p>
-                <b>
-                    <h>
-                        This feature is NOT done. Please check back at a later time.
-                    </h>
-                </b>
-            </p>
+            <form action="/api/v1/createuser" method="POST">
+                <label htmlFor="username">Username:</label>
+                <input type="text" id="username" name="username" /> <br />
+                
+                <label htmlFor="password">Password:</label>
+                <input type="password" id="password" name="password" /><br />
+                
+                <input type="submit" value="Create User" />
+
+                <h2><b>PLEASE NOTE THIS IS NOT WORKING RIGHT NOW</b></h2>
+            </form>
         </div>
-    )
+    );
 }
 
 export default CreateUser;
