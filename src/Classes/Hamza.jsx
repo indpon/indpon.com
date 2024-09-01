7515943f402ea616eebaf0f978b98fa906b17a1f
= <=>
2024-09-01 00:39:15 -0400

    fix add /hamza

--- src/Classes/Hamza.jsx
+++ src/Classes/Hamza.jsx
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
 
 function Hamza() {
-    const [clicks, setClicks] = useState(0);
+  const [clicks, setClicks] = useState(0);
 
-    return (
-        <>
-        <div>
-            <p>
-                <b>
-                    Welcome to Hamzas part of the website, he hasnt told me what he wants in it though
-                </b>
-            </p>
-        </div>
+  return (
+    <>
+      <div>
+        <p>
+          <b>
+            Welcome to Hamzas part of the website, he hasn't told me what he wants in it though
+          </b>
+        </p>
+      </div>
 
-        <div>
-            <button onClick={setClicks(clicks + 1)}>{clicks}</button>
-        </div>
-        </>
-        
-    )
+      <div>
+        <button onClick={() => setClicks(clicks + 1)}>{clicks}</button>
+      </div>
+    </>
+  );
 }
 
 export default Hamza;
