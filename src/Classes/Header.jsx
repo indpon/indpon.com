71165dbcd2a559a4da5839c69b117dab4d47a790
= <=>
2024-09-09 15:36:31 -0400

    fox

--- src/Classes/Header.jsx
+++ src/Classes/Header.jsx
@@ -14,8 +14,19 @@ function Header() {
       <h1>Welcome to indpon.com</h1>
       <nav>
         <div className="hamburger-icon" onClick={toggleMobileMenu}>
+          {/* You can add your hamburger icon here */}
           ☰ 
         </div>
+
+        {/* Original nav links, now always visible */}
+        <div className="nav-links"> {/* Add a class for styling */}
+          <Link to="/about">About</Link>
+          <Link to="/create/user">Create User</Link>
+          <Link to="/todo">Indpon Todo List</Link>
+          <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/K5rxmxvT83">Indpon's discord server</a>
+        </div>
+
+        {/* Mobile menu, conditionally shown */}
         <div className={`mobile-menu ${isMobileMenuOpen ? 'open' : ''}`}>
           <Link to="/about">About</Link>
           <Link to="/create/user">Create User</Link>
